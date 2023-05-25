f13f52967a93e2722430e62eade27060dcd6fafd
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-05-26 01:49:58 +0700

    memperbaiki redirect yang salah

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -20,7 +20,7 @@
                         </div>
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
-                            <a href="{{ route('todo.index') }}"
+                            <a href="{{ route('category.index') }}"
                                 class="inline-flex items-center px-4 py-2 text-xs font-semibold tracking-widest
                                             text-gray-700 uppercase transition duration-150 ease-in-out
                                             bg-white border border-gray-300 rounded-md shadow-sm dark:bg-gray-800
