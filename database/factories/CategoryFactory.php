d0ce8b438c1bf69ab266bfdc81fc3807e554bdf5
Naufal Rozan <naufal.rozan.ft20@mail.umy.ac.id>
2023-05-26 01:13:35 +0700

    CategoryFactory, TodoFactory, & DatabaseSeeder

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Todo>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
  */
-class TodoFactory extends Factory
+class CategoryFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -17,10 +17,8 @@ class TodoFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => ucwords(fake()->sentence()),
+            'title'   => ucwords(fake()->sentence()),
             'user_id' => rand(1, 100),
-            'is_complete' => rand(0, 1),
-            'category_id' => rand(1, 100),
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
+use App\Models\Category;
 use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -27,6 +28,8 @@ class DatabaseSeeder extends Seeder
             ]
             );
             User::factory(100)->create();
+            Category::factory(200)->create();
             Todo::factory(500)->create();
+
     }
 }
